3ffd9e75b5dcce3d4d073d5101d2dc332a31d82c
aswinjayaji <aswinjayaji@cet.ac.in>
2021-08-26 21:23:22 +0530

    added signinup screen

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/constants.dart';
+import 'package:chat_app/screens/signin_or_signup_screen/signin_or_signup_screen.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatelessWidget {
@@ -31,36 +32,40 @@ class WelcomeScreen extends StatelessWidget {
                     .withOpacity(0.64),
               ),
             ),
-            Spacer(flex: 3),
+            Spacer(flex: 2),
             FittedBox(
-                child: TextButton(
-                    onPressed: () {},
-                    child: Row(
-                      children: [
-                        Text(
-                          "Skip",
-                          textAlign: TextAlign.center,
-                          style:
-                              Theme.of(context).textTheme.bodyText1!.copyWith(
-                                    color: Theme.of(context)
-                                        .textTheme
-                                        .bodyText1!
-                                        .color!
-                                        .withOpacity(0.8),
-                                  ),
-                        ),
-                        SizedBox(width: kDefaultPadding / 4),
-                        Icon(
-                          Icons.arrow_forward_ios,
-                          size: 16,
-                          color: Theme.of(context)
-                              .textTheme
-                              .bodyText1!
-                              .color!
-                              .withOpacity(0.8),
-                        )
-                      ],
-                    )))
+              child: TextButton(
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => SigninOrSignupScreen())),
+                child: Row(
+                  children: [
+                    Text(
+                      "Skip",
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                            color: Theme.of(context)
+                                .textTheme
+                                .bodyText1!
+                                .color!
+                                .withOpacity(0.8),
+                          ),
+                    ),
+                    SizedBox(width: kDefaultPadding / 4),
+                    Icon(
+                      Icons.arrow_forward_ios,
+                      size: 16,
+                      color: Theme.of(context)
+                          .textTheme
+                          .bodyText1!
+                          .color!
+                          .withOpacity(0.8),
+                    )
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
